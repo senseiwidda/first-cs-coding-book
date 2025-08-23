316fcdbe062dcee24c2004794456ba71cc6b5406
senseiwidda <happy2sean@gmail.com>
2025-08-23 09:42:21 -0700

    first python

---
+++ python/1-2-list.py
@@ -0,0 +1,12 @@
+# Example of creating a list in Python
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Accessing elements
+print(fruits[0])  # Output: apple
+
+# Adding an element
+fruits.append("orange")
+print(fruits)
+
+# Removing an
