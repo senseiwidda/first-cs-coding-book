fe041ed70117ca96ced942d3190804a1440ea92d
senseiwidda <happy2sean@gmail.com>
2026-02-01 11:53:46 -0800

    comment

--- 1-56-NestedDictionaries.py
+++ 1-56-NestedDictionaries.py
@@ -14,6 +14,27 @@ myfamily ={
     },
 }
 print(myfamily)
-
-
- 
+child1 ={
+    "name": "Emil",
+    "year": 2004
+}
+child2={
+    "name": "Tobias",
+    "year": 2007
+}
+child3={
+    "name": "Linus",
+    "year": 2011    
+}
+herfamily = {
+    "child1": child1,
+    "child2": child2,
+    "child3": child3
+}
+print(herfamily)
+print(herfamily["child2"]["name"])
+for x, ob in herfamily.items():
+    print(x)
+    
+    for y in ob:
+        print(y + ':', ob[y])
