4af7b0bcc4898593460ce0ccb46e2e219b11823b
senseiwidda <happy2sean@gmail.com>
2026-02-04 20:06:07 -0800

    comment

--- W3schools/1-13-Slicingstrings.py
+++ W3schools/1-13-Slicingstrings.py
@@ -1,4 +1,10 @@
 b="Hello, World!"
 print(b[2:5])
 print(b[:5])
-print(b[-5:-2])
+print(b[-1])
+
+c="hello world"
+print(c[2:9])
+print(c[2:9][-1])
+print(c[2:9][-1])
+print(c[6:11])
---
+++ W3schools/1-68-FunctionArguments.py
@@ -0,0 +1,63 @@
+def my_function(fname):
+    print(fname+"Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+def my_function(name):
+    print("Hello", name)
+
+my_function("Emil")
+def my_function(fname, Iname):
+    print(fname+""+Iname)
+
+my_function("Emil", "Refsnes")
+def my_function(name="friend"):
+    print("Hello",name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
+
+def my_function(country="Norway"):
+    print("I am from", country)
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+print("Don't you notice how I get quiet when there's no one else around? ")
+print("Me and you and awkward silence. Don't you dare look at me that way")
+print("I don't need reminders of how you don't feel the same")
+print("Oh, the burning pain")
+print("Listening to you harp on 'bout some new soulmate")
+print("She's so perfect, blah blah blah")
+print("Oh, how I wish you'll wake up one day")
+print("Run to me, confess your love, at least just let me say")
+print("That when I talk to you, oh, Cupid walks right through")
+print("And shoots an arrow through my heart")
+print("And I sound like a loon, but don't you feel it too?")
+print("Confess I loved you from the start")
+print("What's a girl to do?")
+print("Lying on my bed, staring into the blue")
+print("Unrequited, terrifying")
+print("Love is driving me a bit insane")
+print("Have to get this off my chest")
+print("I'm telling you today")
+print("That when I talk to you, oh, Cupid walks right through")
+print("And shoots an arrow through my heart")
+print("And I sound like a loon, but don't you feel it too?")
+print("Confess I loved you from the start")
+print("Confess I loved you. Just thinking of you. I know I've loved you from the start")
+def my_function(animal, name):
+    print("I have a", animal)
+    print("My", animal+"'s name is", name)
+my_function(animal="dog",name="Buddy")
+def my_function(animal,name):
+    print("I have a", animal)
+    print("My", animal+"'s name is", name)
+
+my_function(name="Buddy", animal="dog")
+def my_function(animal,name):
+    print("I have a", animal)
+    print("My", animal)
